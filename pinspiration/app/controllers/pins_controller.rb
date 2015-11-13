51b421a546ef70f6104a7be3c2345acc7d1146df
Timothy Gregg <timtgregg@gmail.com>
2015-11-13 11:04:10 -0500

    full crud for pins

--- pinspiration/app/controllers/pins_controller.rb
+++ pinspiration/app/controllers/pins_controller.rb
@@ -17,15 +17,37 @@ class PinsController < ApplicationController
   end
 
   def create
-    
+    @user = current_user
+    @pin = @user.pins.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin
+    else
+      render 'new'
+    end
+
   end
 
   def update
+    @pin = Pin.find(params[:id])
+
+    if @pin.update(pin_params)
+      redirect_to @pin
+    else
+      render 'edit'
+    end
 
   end
 
   def destroy
-
+    @pin = Pin.find(params[:id])
+    @pin.destroy
+    redirect_to pins_path
   end
+
+  private
+    def pin_params
+      params.require(:pin).permit(:title, :img_url)
+    end
 
 end
