95632752318449bda57a19b002718e0992619f4b
Markham Shofner <markham.shofner@gmail.com>
2015-11-13 13:45:05 -0500

    streamline boards/index page

--- pinspiration/db/board_data.rb
+++ pinspiration/db/board_data.rb
@@ -6,14 +6,9 @@ def get_board_data
       description: "These are my favorite things that fly.",
     },
     "ThingsThatSwim" => {
-      name: "ThingsThatSwim",
+      name: "Things That Swim",
       description: "There are my favorite things that swim.",
     }
   }
-    # "WithUser_IDTest" => {
-    #   name: "With User_ID Test",
-    #   description: ,
-    #   user_id: 1,
-    # }
   return board_data
 end
