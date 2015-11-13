825fc1742fdf4655c2452b4cb585775cc12ed271
Timothy Gregg <timtgregg@gmail.com>
2015-11-13 11:31:28 -0500

    added board model and controller

--- pinspiration/config/routes.rb
+++ pinspiration/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pins#index"
-  resources :pins
+
+  root to: "boards#index"
+
+  resources :boards do
+    resources :pins
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
