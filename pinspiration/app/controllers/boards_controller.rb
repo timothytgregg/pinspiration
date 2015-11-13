6f2911341580fdaf152541f0c1d87ab4757b0c08
Timothy Gregg <timtgregg@gmail.com>
2015-11-13 12:02:20 -0500

    board paths and displays working, nested pin paths need work

--- pinspiration/app/controllers/boards_controller.rb
+++ pinspiration/app/controllers/boards_controller.rb
@@ -1,2 +1,53 @@
 class BoardsController < ApplicationController
+
+  def index
+    @boards=Board.all
+  end
+
+  def show
+    @board=Board.find(params[:id])
+  end
+
+  def new
+    @board=Board.new
+  end
+
+  def edit
+    @board=Board.find(params[:id])
+  end
+
+  def create
+    @user = current_user
+    @board = @user.boards.new(board_params)
+
+    if @board.save
+      redirect_to @board
+    else
+      render 'new'
+    end
+
+  end
+
+  def update
+    @board = Board.find(params[:id])
+
+    if @board.update(board_params)
+      redirect_to @board
+    else
+      render 'edit'
+    end
+
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
+  end
+
+  private
+    def board_params
+      params.require(:board).permit(:name, :description)
+    end
+
 end
