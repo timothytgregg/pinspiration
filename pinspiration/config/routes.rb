0ed41587f4d9452f9de5e45d1ac945a8c169b34b
Timothy Gregg <timtgregg@gmail.com>
2015-11-13 10:43:32 -0500

    index and show views displaying well

--- pinspiration/config/routes.rb
+++ pinspiration/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "posts#index"
+  root to: "pins#index"
   resources :pins
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
