b9262f71ccf66e6bd168a759cc8ed73f9acbb15c
Markham Shofner <markham.shofner@gmail.com>
2015-11-13 12:04:48 -0500

    bones of new seed data setup for boards and pins

---
+++ pinspiration/db/board_data.rb
@@ -0,0 +1,17 @@
+def get_board_data
+  board_data = {
+    "ThingsThatFly" => {
+      name: "Things That Fly",
+      description: "These are my favorite things that fly.",
+      }
+    "ThingsThatSwim" => {
+      name: "ThingsThatSwim",
+      description: "There are my favorite things that swim.",
+    }
+    # "WithUser_IDTest" => {
+    #   name: "With User_ID Test",
+    #   description: ,
+    #   user_id: 1,
+    # }
+  return board_data
+end
---
+++ pinspiration/db/pin_data.rb
@@ -0,0 +1,16 @@
+def get_pin_data
+  pin_data =
+    {"ThingsThatFly"=>
+      [{:title=>"First Flyer",
+      :img_url=>"http://nyulocal.com/wp-content/uploads/2013/10/The-plane-flying-at-sunset-airliner-photography_1920x1080.jpg"},
+      {:title=>"Second Flyer",
+      :img_url=>"http://pad1.whstatic.com/images/thumb/f/f0/Draw-a-Shark-Step-8-Version-3.jpg/670px-Draw-a-Shark-Step-8-Version-3.jpg"}]
+    },
+    {"ThingsThatSwim"=>
+      [{:title=>"First Swimmger",
+      :img_url=>"http://nyulocal.com/wp-content/uploads/2013/10/The-plane-flying-at-sunset-airliner-photography_1920x1080.jpg"},
+      {:title=>"Second Swimmger",
+      :img_url=>"http://pad1.whstatic.com/images/thumb/f/f0/Draw-a-Shark-Step-8-Version-3.jpg/670px-Draw-a-Shark-Step-8-Version-3.jpg"}]
+    },
+  return pin_data
+end
